86ea90224d0a1a325c51c874a85396044d752dd7
seymanurterzioglu <nightotaku00@hotmail.com>
2023-05-17 13:04:50 +0300

    .

--- src/homeworks/day2/abstractClass/GameCalculator.java
+++ src/homeworks/day2/abstractClass/GameCalculator.java
@@ -2,7 +2,7 @@ package homeworks.day2.abstractClass;
 
 // abstract class da abstract metot olmak zorunda değil
 // abstract fonk sahip class da abstract olmak zorundadır
-//
+// abstract classlardan new ile nesne tanımlanamaz
 
 public abstract class GameCalculator {
 	
